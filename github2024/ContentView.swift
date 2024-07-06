a20916ba8ccc0cadfd721e619b8bc6d7925cc002
Angeline Lee <angelinelee513@gmail.com>
2024-07-06 15:57:57 +0800

    Initial Commit

---
+++ github2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  github2024
+//
+//  Created by T Krobot on 6/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ github2024/github2024App.swift
@@ -0,0 +1,17 @@
+//
+//  github2024App.swift
+//  github2024
+//
+//  Created by T Krobot on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct github2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
